1701f5a9aa254d08177ac333566af19bebc87046
tyche0529 <tyche0529@gmail.com>
2023-07-25 09:12:21 +0900

    대시보드 디자인 일부 적용

--- src/privatePage/PrivateRouter.tsx
+++ src/privatePage/PrivateRouter.tsx
@@ -5,10 +5,10 @@ import Dashboard from './Dashboard';
 export default function PrivateRouter(){
     return(
         <div className='flex flex-grow w-screen'>
-            <div className='basis-1/5'>
+            <div className='basis-1/5 h-screen bg-aliceblue'>
                 <SideMenu/>
             </div>
-            <div className='basis-4/5'>
+            <div className='basis-4/5 h-screen'>
                 <Dashboard/>
             </div>
         </div>
--- src/privatePage/SideMenu.tsx
+++ src/privatePage/SideMenu.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import logoTriangle from '../static/img/Switch-triangle.png';
 
 export default function SideMenu(){
     return (
-        <div>
-            sideBar
+        <div className="bg-cyan-50 h-full">
+            <div className="bg-cyan-600	 flex justify-start content-center decoration-white">
+                <img className="w-24 h-24" src={logoTriangle}/>
+                <div className="">Switch Project</div>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 }
