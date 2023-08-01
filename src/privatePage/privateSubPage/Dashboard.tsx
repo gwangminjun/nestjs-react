e6debb258a343db3ffc73581092e86cb0d6b52a7
양민준 <97660495+gwangminjun@users.noreply.github.com>
2023-08-02 00:41:46 +0900

    chart,js 확인중
    chart,js 확인중

--- src/privatePage/privateSubPage/Dashboard.tsx
+++ src/privatePage/privateSubPage/Dashboard.tsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  ArcElement,
+  RadialLinearScale,
+} from "chart.js";
+import {
+  Line,
+  Bar,
+  Doughnut,
+  Radar,
+  Pie,
+  Bubble,
+  PolarArea,
+} from "react-chartjs-2";
 
-export default function Dashboard(){
-    return(
-        <div className='h-9/10'>
-            Dashboard
-        </div>
-    )
+// 라이브러리 등록
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const one = ["1", "4", "21", "14", "23", "43", "66"];
+const two = ["2", "5", "3", "15", "32", "55", "24"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: one,
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: two,
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export default function Dashboard() {
+  return (
+    <div className="h-9/10">
+      <Line data={data} options={options} />
+      <Bar data={data} options={options} />
+      <Radar data={data} options={options} />
+      <Pie data={data} options={options} />
+      <PolarArea data={data} options={options} />
+      <Bubble data={data} options={options} />
+      <Doughnut data={data} options={options} />
+    </div>
+  );
 }
